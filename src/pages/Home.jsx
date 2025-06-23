47b73de0e475dcc082774c376621e0e2c6d3a01c
nithyaganesh-dev <nithyaganesh.developer@gmail.com>
2025-06-23 18:27:28 +0530

    Implement Testimonials Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import BgSlider from "../components/BgSlider"
 import Header from "../components/Header"
 import Steps from "../components/Steps"
+import Testimonials from "../components/Testimonials"
 
 const Home = () => {
   return (
     <>
       <Header />
       <Steps />
-      <BgSlider/>
+      <BgSlider />
+      <Testimonials />
     </>
   )
 }
