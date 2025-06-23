c9d2346be74d3648233dd8efc223551ae6b668ea
nithyaganesh-dev <nithyaganesh.developer@gmail.com>
2025-06-23 20:00:07 +0530

    Implement Clerk User Signup/Login

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer"
 const App = () => {
   return (
     <div className="min-h-screen bg-slate-50">
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom"
 import { assets } from "../assets/assets.js"
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react"
 
 const Navbar = () => {
+    const { openSignIn } = useClerk()
+    const { isSignedIn, user } = useUser()
+
     return (
         <nav className="flex items-center justify-between mx-4 py-3 lg:mx-44 ">
             <Link to="/">
                 <img className="w-32 sm:w-44" src={assets.logo} alt="Logo" />
             </Link>
-            <button className="bg-zinc-800 text-white flex items-center gap-4
+            {
+                isSignedIn
+                    ?
+                    <div>
+                        <UserButton />
+                    </div>
+                    :
+                    <button onClick={() => openSignIn()} className="bg-zinc-800 text-white flex items-center gap-4
             px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full cursor-pointer">
-                Get Started
-                <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="Arrow Icon" />
-            </button>
+                        Get Started
+                        <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="Arrow Icon" />
+                    </button>
+
+            }
         </nav>
     )
 }
